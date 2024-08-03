95316212b55d7e67211164acf9fc90578a96f7f4
LilDojd <yawner@protonmail.com>
2024-08-03 15:03:56 +0400

    :wrench:(app): WIP

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "all",
+  content: [
+    // include all rust, html and css files in the src directory
+    "./src/**/*.{rs,html,css}",
+    // include all html files in the output (dist) directory
+    "./dist/**/*.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "nasty-black": "#1a1a1a",
+      },
+    },
+  },
+  plugins: [],
+};
