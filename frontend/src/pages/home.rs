9614ca9f29215903384449f4eea7ae389a3f995f
LilDojd <yawner@protonmail.com>
2024-08-03 09:04:51 +0400

    :wrench:(front): Trying out dx 0.6 alpha

--- frontend/src/pages/home.rs
+++ frontend/src/pages/home.rs
@@ -17,16 +17,16 @@ pub fn Home() -> Element {
             }
             main {
                 class: "relative z-10 flex flex-col items-center justify-center min-h-screen text-center px-4",
-                h1 { class: "text-4xl font-bold mb-2", "Georgiy Andreev" }
+                h1 { class: "text-4xl font-bold mb-2", "George" }
                 p { class: "text-xl mb-8", "Software Engineer at GABioInf" }
                 p { class: "max-w-2xl mb-8",
                     "Building polished software experiences with magical, unique and delightful details, for the web. I aim to create beautiful and functional software that is both intuitive and enjoyable for users."
                 }
                 p { class: "mb-8 max-w-2xl",
-                    "I have a passion for learning, and I am constantly seeking to improve my skills mostly through ",
-                    span { class: "underline", "reading" },
-                    " and ",
-                    span { class: "underline", "writing" },
+                    "I have a passion for learning, and I am constantly seeking to improve my skills mostly through "
+                    span { class: "underline", "reading" }
+                    " and "
+                    span { class: "underline", "writing" }
                     ". I'm interested in Rust and at the same time, I'm also experimenting with other technologies."
                 }
                 div { class: "flex space-x-4",
