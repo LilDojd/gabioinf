ea8bee5ced8394b6fbe05c10dc82f5ec9af17f2b
LilDojd <yawner@pm.me>
2024-08-18 08:01:21 +0400

    :sparkles:(signature_list): increase signatures per page and add intersection observer
    This commit includes the following changes:
    
    - Increase the number of signatures displayed per page from 2 to 8
    - Implement an intersection observer to automatically load more signatures when the user scrolls to the bottom of the page
    - Set the intersection threshold to 0.5 to trigger the loading of more signatures before the user reaches the bottom of the page

--- src/components/signature_list.rs
+++ src/components/signature_list.rs
@@ -2,7 +2,8 @@ use crate::components::{Card, CardType};
 use crate::shared::{models::GuestbookEntry, server_fns};
 use dioxus::prelude::*;
 
-const SIGNATURES_PER_PAGE: usize = 2;
+const SIGNATURES_PER_PAGE: usize = 8;
+const INTERSECTION_THRESHOLD: f64 = 0.5;
 
 #[derive(Clone, Copy, PartialEq, Debug, Default)]
 pub enum SignatureListState {
@@ -19,7 +20,61 @@ pub fn SignatureList(refresh_trigger: Signal<bool>) -> Element {
     let endless_signatures = use_signal(|| vec![]);
     let (load_next_batch, mut refresh) = use_signature_list(load_state, endless_signatures);
 
-    // Watch for changes in the refresh_trigger
+    // Use the intersection observer
+    let loader_ref = use_signal(|| format!("signature-loader-{}", rand::random::<u32>()));
+
+    let mut is_intersecting = use_signal(|| false);
+
+    use_effect(move || {
+        // Create and run the Intersection Observer
+        let mut eval = eval(
+            format!(
+                r#"
+            const callback = (entries, observer) => {{
+                entries.forEach((entry) => {{
+                    dioxus.send(entry.isIntersecting);
+                }});
+            }};
+
+            const options = {{ root: null, threshold: {INTERSECTION_THRESHOLD} }};
+            const observer = new IntersectionObserver(callback, options);
+
+            const target = document.getElementById('{loader_ref}');
+            console.log(target);
+            if (target) {{
+                observer.observe(target);
+            }}
+
+            // Cleanup function
+            () => {{
+                if (target) {{
+                    observer.unobserve(target);
+                }}
+            }}
+            "#
+            )
+            .as_ref(),
+        );
+
+        to_owned![is_intersecting];
+        spawn(async move {
+            while let Ok(is_intersecting_js) = eval.recv().await {
+                if let Some(value) = is_intersecting_js.as_bool() {
+                    dioxus_logger::tracing::info!("Set intersecting to {}", value);
+                    is_intersecting.set(value);
+                }
+            }
+        });
+    });
+
+    use_effect(move || {
+        if *is_intersecting.read()
+            && matches!(*load_state.read(), SignatureListState::MoreAvailable(_))
+        {
+            load_next_batch.send(());
+        }
+    });
+
     use_effect(move || {
         let _ = refresh_trigger();
         refresh();
@@ -28,25 +83,29 @@ pub fn SignatureList(refresh_trigger: Signal<bool>) -> Element {
     rsx! {
         div {
             div { class: "grid grid-cols-1 md:grid-cols-2 gap-6",
-                {endless_signatures.read().iter().flatten().map(|entry| rsx! {
-                    Card { card_type: CardType::Signature(entry.clone()) }
-                })}
+                {
+                    endless_signatures
+                        .read()
+                        .iter()
+                        .flatten()
+                        .map(|entry| rsx! {
+                            Card { card_type: CardType::Signature(entry.clone()) }
+                        })
+                }
             }
-
-            match *load_state.read() {
-                SignatureListState::Initial | SignatureListState::Loading => rsx! {
-                    div { class: "text-center py-4", "Loading signatures..." }
-                },
-                SignatureListState::MoreAvailable(_) => rsx! {
-                    button {
-                        class: "mt-4 px-4 py-2 bg-jet text-stone-100 rounded hover:bg-onyx",
-                        onclick: move |_| load_next_batch.send(()),
-                        "Load more..."
+            div { id: "{loader_ref}", class: "h-10", "test" }
+            div {
+                match *load_state.read() {
+                    SignatureListState::Initial | SignatureListState::Loading => {
+                        rsx! {
+                            div { class: "text-center py-4", "Loading signatures..." }
+                        }
                     }
-                },
-                SignatureListState::Finished => rsx! {
-                    div { class: "text-center py-4", "No more signatures to load" }
-                },
+                    SignatureListState::MoreAvailable(_) => rsx! {  },
+                    SignatureListState::Finished => rsx! {
+                        div { class: "text-center py-4", "No more signatures to load" }
+                    },
+                }
             }
         }
     }
@@ -105,4 +164,55 @@ fn use_signature_list(
     };
 
     (next_task, refresh)
+}
+
+pub fn use_intersection_observer(element_id: String, threshold: f64) -> Signal<bool> {
+    let is_intersecting = use_signal(|| false);
+
+    use_effect(move || {
+        // Create and run the Intersection Observer
+        let mut eval = eval(
+            format!(
+                r#"
+            const callback = (entries, observer) => {{
+                entries.forEach((entry) => {{
+                    if (entry.isIntersecting == true) {{
+                        console.log("Intersecting");
+                        console.log(entry);
+                        dioxus.send(entry.isIntersecting);
+                    }}
+                }});
+            }};
+
+            const options = {{ root: null, threshold: {threshold} }};
+            const observer = new IntersectionObserver(callback, options);
+
+            const target = document.getElementById('{element_id}');
+            console.log(target);
+            if (target) {{
+                observer.observe(target);
+            }}
+
+            // Cleanup function
+            () => {{
+                if (target) {{
+                    observer.unobserve(target);
+                }}
+            }}
+            "#
+            )
+            .as_ref(),
+        );
+
+        to_owned![is_intersecting];
+        spawn(async move {
+            while let Ok(is_intersecting_js) = eval.recv().await {
+                if let Some(value) = is_intersecting_js.as_bool() {
+                    is_intersecting.set(value);
+                }
+            }
+        });
+    });
+
+    is_intersecting
 }
