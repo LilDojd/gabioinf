e870d7a87e9ba991d51579952ae03dc2bc58ea00
LilDojd <yawner@protonmail.com>
2024-08-19 15:45:38 +0400

    :sparkles:(load_signatures): add ability to load guestbook signaturese
    This commit adds a new server function `load_signatures` that fetches
    all the guestbook entries from the database and returns them to the
    client.
    
    The function uses the `GuestbookEntryCriteria` struct to fetch the
    entries, and the `Repository` trait to interact with the database.
    
    The function returns a `Result<Vec<GuestbookEntry>, ServerFnError>`,
    where the error case handles any errors that may occur during the
    database query.
    
    Closes #32

--- src/shared/server_fns/guestbook/load_signatures.rs
+++ src/shared/server_fns/guestbook/load_signatures.rs
@@ -1,8 +1,12 @@
 use dioxus::prelude::*;
 
 #[cfg(feature = "server")]
-use crate::backend::AppState;
-use crate::shared::models::GuestbookEntry;
+use crate::backend::{
+    errors::ApiError,
+    repos::{GuestbookEntryCriteria, Repository},
+    AppState,
+};
+use crate::shared::models::{Guest, GuestbookEntry};
 
 #[server(LoadSignatures)]
 pub async fn load_signatures(
@@ -16,4 +20,23 @@ pub async fn load_signatures(
     let signatures = guestbook_repo.read_page(page, per_page).await?;
 
     Ok(signatures)
+}
+
+#[server(LoadUserSignature)]
+pub async fn load_user_signature(user: Guest) -> Result<Option<GuestbookEntry>, ServerFnError> {
+    let FromContext(state): FromContext<AppState> = extract().await?;
+
+    let guestbook_repo = state.guestbook_repo;
+
+    let signature = guestbook_repo
+        .read(&GuestbookEntryCriteria::WithAuthorId(user.id))
+        .await;
+
+    match signature {
+        Ok(signature) => Ok(Some(signature)),
+        Err(e) => match e {
+            ApiError::DatabaseError(sqlx::Error::RowNotFound) => Ok(None),
+            _ => Err(ServerFnError::ServerError(e.to_string())),
+        },
+    }
 }
