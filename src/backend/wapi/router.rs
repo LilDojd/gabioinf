70c32860f09cd0492a01df8ca3f1ca1a8651a609
LilDojd <yawner@protonmail.com>
2025-06-26 23:47:14 +0400

    Tweak csp

--- src/backend/wapi/router.rs
+++ src/backend/wapi/router.rs
@@ -79,13 +79,7 @@ pub fn api_router(
                 if res.headers().get("content-security-policy").is_none() {
                     res.headers_mut().insert(
                         "content-security-policy",
-                        generate_default_csp()
-                            .to_string()
-                            .parse()
-                            .unwrap_or_else(|_| {
-                                dioxus_logger::tracing::error!("Failed to parse default CSP");
-                                HeaderValue::from_static(fallback_static_str_csp())
-                            }),
+                        generate_default_csp().to_string().parse().unwrap(),
                     );
                 }
                 res
@@ -93,7 +87,6 @@ pub fn api_router(
             .into_inner(),
     )
 }
-/// Returns a default configuration of http security headers.
 fn generate_general_helmet_headers() -> Helmet {
     Helmet::new()
         .add(CrossOriginOpenerPolicy::same_origin())
@@ -112,8 +105,6 @@ fn generate_general_helmet_headers() -> Helmet {
         .add(XPermittedCrossDomainPolicies::none())
         .add(XXSSProtection::off())
 }
-/// Returns a default strict Content Security Policy.
-/// It's used whenever a custom CSP is not set.
 fn generate_default_csp() -> ContentSecurityPolicy<'static> {
     ContentSecurityPolicy::new()
         .default_src(vec!["'self'"])
@@ -122,27 +113,11 @@ fn generate_default_csp() -> ContentSecurityPolicy<'static> {
         .form_action(vec!["'none'"])
         .frame_src(vec!["'none'"])
         .frame_ancestors(vec!["'none'"])
-        .img_src(vec!["'none'"])
         .object_src(vec!["'none'"])
-        .script_src(vec!["'none'"])
-        .style_src(vec!["'none'"])
+        .script_src(vec!["'self'", "'wasm-unsafe-eval'"])
+        .style_src(vec!["'self'", "'unsafe-inline'"])
+        .img_src(vec!["'self'", "data:", "blob:"])
+        .connect_src(vec!["'self'", "https://api.github.com"])
         .worker_src(vec!["'none'"])
         .upgrade_insecure_requests()
-}
-/// Returns a default strict Content Security Policy as a static string.
-const fn fallback_static_str_csp() -> &'static str {
-    "
-    default-src 'self';
-    base-uri 'none';
-    font-src 'none';
-    form-action 'none';
-    frame-src 'none';
-    frame-ancestors 'none';
-    img-src 'none';
-    object-src 'none';
-    script-src 'none';
-    style-src 'none';
-    worker-src 'none';
-    upgrade-insecure-requests;
-    "
 }
